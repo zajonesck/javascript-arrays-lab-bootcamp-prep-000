5f52d504c311244e14ce0df2289091533bffe6c4
Zachary Clinton Jones <zajonesck83@gmail.com>
2018-02-12 18:50:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var kittens = ["Milo", "Otis", "Garfield"]
-Function destructivelyAppendKitten(kittens, Walter) {
+function destructivelyAppendKitten(kittens, Walter) {
   kittens.unshift (Walter);
   return kittens;
 }
